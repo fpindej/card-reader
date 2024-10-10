465e909f17d0966a92b7b94b3622b6dd71af0072
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-10-11 01:26:11 +0200

    Create boilerplate RFID controller

---
+++ src/CardReader.WebApi/Controllers/RfidController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CardReader.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RfidController : ControllerBase
+{
+    private readonly ILogger<RfidController> _logger;
+
+    public RfidController(ILogger<RfidController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] RfidRequest? request)
+    {
+        if (request is null)
+        {
+            _logger.LogWarning("Received empty RFID request");
+            return BadRequest();
+        }
+
+        if (string.IsNullOrWhiteSpace(request.NuidHex))
+        {
+            _logger.LogWarning("Received empty NUID");
+            return BadRequest();
+        }
+
+        _logger.LogInformation("Received RFID request: {NuidHex}", request.NuidHex);
+
+        return Ok();
+    }
+}
+
+public record RfidRequest(string? NuidHex);
