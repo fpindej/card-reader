d271d5a5def6101345545726c553e4ee5098c945
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-10-26 12:48:49 +0200

    Add Domain project

---
+++ src/CardReader.Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CardReader.Domain;
+
+public class User
+{
+    public required Guid Id { get; init; }
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required ushort YearOfBirth { get; set; }
+}
