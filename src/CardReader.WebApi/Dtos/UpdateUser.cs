d02efe25769e27bd5af51e6b7b4ad90b45fd1ac3
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-10-26 13:03:14 +0200

    Create User controller

---
+++ src/CardReader.WebApi/Dtos/UpdateUser.cs
@@ -0,0 +1,3 @@
+﻿namespace CardReader.WebApi.Dtos;
+
+public record UpdateUserRequest(Guid Id, string FirstName, string LastName, ushort YearOfBirth);
---
+++ src/CardReader.WebApi/Mappings/UserMappings.cs
@@ -0,0 +1,26 @@
+﻿using CardReader.Domain;
+using CardReader.WebApi.Dtos;
+
+namespace CardReader.WebApi.Mappings;
+
+internal static class UserMappings
+{
+    public static User ToDomain(this CreateUserRequest request) => new()
+    {
+        Id = Guid.NewGuid(),
+        FirstName = request.FirstName,
+        LastName = request.LastName,
+        YearOfBirth = request.YearOfBirth
+    };
+
+    public static CreateUserResponse ToResponse(this User user) 
+        => new(user.Id, user.FirstName, user.LastName, user.YearOfBirth);
+
+    public static User ToDomain(this UpdateUserRequest request) => new()
+    {
+        Id = request.Id,
+        FirstName = request.FirstName,
+        LastName = request.LastName,
+        YearOfBirth = request.YearOfBirth
+    };
+}
