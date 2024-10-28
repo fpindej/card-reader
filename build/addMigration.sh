40928a64e260d1af724964d93e806f44847181b7
Filip Pindej <fpindej@users.noreply.github.com>
2024-10-28 01:08:31 +0100

    Add addMigration shell script

---
+++ build/addMigration.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Parameters
+MigrationName=$1
+ProjectPath="../src/CardReader.Infrastructure.Persistence/CardReader.Infrastructure.Persistence.csproj"
+StartupProjectPath="../src/CardReader.WebApi/CardReader.WebApi.csproj"
+
+# Function to execute commands
+execute_command() {
+    local command=$1
+
+    echo "Executing: $command"
+    $command
+    if [ $? -ne 0 ]; then
+        echo "Error: Command exited with code $?"
+        exit $?
+    fi
+}
+
+# Check if MigrationName is provided
+if [ -z "$MigrationName" ]; then
+    echo "Migration name must be provided"
+    exit 1
+fi
+
+echo "Starting migration: $MigrationName"
+
+# Run migration command
+execute_command "dotnet ef migrations add $MigrationName --project $ProjectPath --startup-project $StartupProjectPath"
+
+echo "Migration $MigrationName added successfully."
+echo "Running the project with localDev environment setup should apply the migrations automatically."
