77fa2f02dd3421336a26fbeb0e654be6d7577dcf
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-10-28 11:41:30 +0100

    Add publish and revertMigration scripts

---
+++ build/revertMigration.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ProjectPath="../src/CardReader.Infrastructure.Persistence/CardReader.Infrastructure.Persistence.csproj"
+StartupProjectPath="../src/CardReader.WebApi/CardReader.WebApi.csproj"
+
+execute_command() {
+    command=$1
+    echo "Executing: $command"
+    eval $command
+    status=$?
+    if [ $status -ne 0 ]; then
+        echo "Error: Command exited with code $status"
+        exit $status
+    fi
+}
+
+echo "Reverting the last migration..."
+
+execute_command "dotnet ef migrations remove --project $ProjectPath --startup-project $StartupProjectPath"
+
+echo "Last migration successfully reverted."
