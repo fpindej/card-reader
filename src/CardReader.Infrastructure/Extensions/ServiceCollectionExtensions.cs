070d51e9476c59f61b414d3c49aae18465f911c9
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2025-03-12 19:32:29 +0100

    Remove old services

--- src/CardReader.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/CardReader.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
-﻿using CardReader.Application;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace CardReader.Infrastructure.Extensions;
 
@@ -7,9 +6,6 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddScoped<IUserService, UserService>();
-        services.AddScoped<IRfidCardService, RfidCardService>();
-
         return services;
     }
 }
