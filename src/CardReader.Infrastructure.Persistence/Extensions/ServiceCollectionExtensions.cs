ceab51fd50b4e4e476e51a04e2d5ba580bc6bf9f
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2025-03-13 02:27:09 +0100

    Log connection string initilalization

--- src/CardReader.Infrastructure.Persistence/Extensions/ServiceCollectionExtensions.cs
+++ src/CardReader.Infrastructure.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using CardReader.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CardReader.Infrastructure.Persistence.Extensions;
 
@@ -11,10 +12,14 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<GymDoorDbContext>(opt =>
+        services.AddDbContext<GymDoorDbContext>((sp, opt) =>
         {
             var connectionString = configuration.GetConnectionString("Database");
             opt.UseNpgsql(connectionString);
+            
+            var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger<GymDoorDbContext>();
+            logger.LogInformation("Opening database connection: {connectionString}", connectionString);
         });
         
         services.AddScoped<IUnitOfWork, UnitOfWork>();
