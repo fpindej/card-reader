c07382dd48a0418a013fbbae4ac6121ea0f212de
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2025-03-14 23:54:22 +0100

    Add migrations

--- src/CardReader.Infrastructure.Persistence/Migrations/20250314225105_AddAccessLog.Designer.cs
+++ src/CardReader.Infrastructure.Persistence/Migrations/20250314225105_AddAccessLog.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CardReader.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CardReader.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(GymDoorDbContext))]
-    partial class GymDoorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250314225105_AddAccessLog")]
+    partial class AddAccessLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
